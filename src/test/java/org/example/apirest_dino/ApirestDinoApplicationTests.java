0c98075d8118fc91dd6b01247be212f64ba10ae9
meschinidino <francodmsoloing@gmail.com>
2024-10-15 21:39:30 -0300

    first commit

---
+++ src/test/java/org/example/apirest_dino/ApirestDinoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.apirest_dino;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApirestDinoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
